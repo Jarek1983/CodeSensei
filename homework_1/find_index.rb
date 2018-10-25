e589a85e50bc949c3106923c0ac67e86326109b0
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-10-25 14:46:42 +0200

    Added find_index - homework_1

---
+++ homework_1/find_index.rb
@@ -0,0 +1,11 @@
+
+def find_index(array, value)
+  	return value if array.include? value
+  	return nil if array.none? value
+end
+
+#find_index([1, 2, 3, 4, 5], 3)
+#find_index([5, 8, 6, 2, 2, 10], 2)
+#find_index([10, 20, 30], 100)
+#find_index([], 0)
+
