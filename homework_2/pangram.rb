7ccd5e1dee8f4240677320825d7b760ef0da4403
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-10-27 11:48:26 +0200

    Added pangram - homework_2

---
+++ homework_2/pangram.rb
@@ -0,0 +1,5 @@
+def pangram?(line)
+  ["a","ą","b","c","ć","d","e","ę","f","g","h","i","j","k","l","ł","m","n","ń",
+  	"o", "ó","p","r","s","ś","t","u","w","y","z","ź","ż"].all? { |word| 
+  	line.downcase.include? (word) }
+end
