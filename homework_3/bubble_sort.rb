8c55ab2b4f98df28131d021efc419e4ddafec766
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-11-11 12:57:21 +0100

    Added bubble_sort

---
+++ homework_3/bubble_sort.rb
@@ -0,0 +1,30 @@
+class Bubble
+
+  def initialize(*array)
+    @array = array.map(&:to_i)
+  end
+
+  def sort
+    @array.length.times do |x|
+      @array.length.times do |i|
+        if i+1 < @array.length && @array[i+1] < @array[i]
+          @array[i], @array[i+1] = @array[i+1], @array[i]
+        end
+      end
+    end
+     @array
+  end
+
+end
+
+if ARGV.empty?
+  puts "Podaj argumenty"
+else
+  score = Bubble.new(*ARGV)
+ puts "Wynik: #{score.sort.join(", ")}"
+end
+
+first = Bubble.new
+
+
+#  a = [74, 77, 64, 19, 43, 47, 77, 66, 47, 60, 7, 97, 71, 87, 95, 79]
