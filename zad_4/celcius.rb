c9186fae159a84fb1bd2aad4e6f10286ad4e09c4
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-11-23 18:22:27 +0100

    Added rails blog - homework_4

--- zad_4/celcius.rb
+++ zad_4/celcius.rb
@@ -1,3 +1,8 @@
+#Stwórz klasę Celcius, która przyjmuję temperaturę jako parametr.
+#Zdefiniuj metodę, która zwraca temperaturę w Fahrenheitach. Do konwersji możemy zastosować wzór temperatura*1.8 + 32. Zaokrąglij wynik tak by zwracał zawsze liczby całkowite
+#Stwórz metodę to_s, która zwraca łańcuch znaków z temperaturą, przykład: "16 degrees C"
+
+
 class Celcius
 	attr_accessor :temperature
 
